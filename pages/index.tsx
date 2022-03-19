21a6bc3fbbb1aaa85bdf6e448f96e7a32fdaee03
Izarothy <izarothmail@gmail.com>
2022-03-19 16:34:45 +0100

    setup: graphCMS integration

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,53 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 
-const Home: NextPage = () => {
+import { Post } from 'utils/types';
+
+// GraphCMS setup
+import { gql, GraphQLClient } from 'graphql-request';
+const graphcms = new GraphQLClient(process.env.API_URL!);
+const QUERY = gql`
+  {
+    posts {
+      id
+      title
+      content
+    }
+  }
+`;
+
+export const getStaticProps = async () => {
+  const { posts } = await graphcms.request(QUERY);
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
+
+type Props = {
+  posts: Post[];
+};
+
+const Home: NextPage<Props> = ({ posts }) => {
   return (
     <div>
       <Head>
         <title>Blog App</title>
       </Head>
+      <main>
+        {posts.map((post) => {
+          return (
+            <div key={post.id}>
+              <h1 className="text-4xl">{post.title}</h1>
+              {post.content.split('\n').map((line, idx) => {
+                return <p key={idx}>{line}</p>;
+              })}
+            </div>
+          );
+        })}
+      </main>
     </div>
   );
 };
